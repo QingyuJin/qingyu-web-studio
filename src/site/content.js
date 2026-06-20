50ca3b64fbd8f3d7aea034407caef88b2487c5aa
Jin Qingyu <a0988874324@gmail.com>
2026-06-20 17:53:36 +0800

    Update public LINE contact ID

--- src/site/content.js
+++ src/site/content.js
@@ -2,7 +2,7 @@ export const siteUrl = "https://www.qingyuweb.com"
 
 export const contact = {
   email: "a0988874324@gmail.com",
-  lineId: "a0988874324",
+  lineId: "mulavuc",
   line: "目前先提供 LINE ID 複製與 Email 聯絡。你可以把產業、想做的功能、預算區間與希望上線時間一起傳給我。",
   github: "https://github.com/QingyuJin/qingyu-web-studio",
 }
